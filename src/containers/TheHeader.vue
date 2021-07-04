051026c19e2a0cd4a210dcae4fce3acbaff0acb4
Dragon <rhs0219@hotmail.com>
2021-07-04 18:05:56 +0800

    udpate header, menu, logo

---
+++ src/containers/TheHeader.vue
@@ -0,0 +1,88 @@
+<template>
+  <CHeader class=" headerStyle" with-subheader>
+  <img src="icon.png" class="mx-3" v-if="iconflag" />  
+    <CToggler
+      in-header
+      class="ml-3 d-lg-none headerFont"
+      @click="$store.commit('toggleSidebarMobile')"
+    />
+    <CToggler
+      in-header
+      class="ml-3 d-md-down-none headerFont"
+      @click="$store.commit('toggleSidebarDesktop')"
+    />
+    {{show}}
+    <CHeaderBrand class="mx-auto d-lg-none" to="/">
+       <img src="logo.png"  class="c-sidebar-brand-minimized" style="width:50%"/>         
+    </CHeaderBrand>
+    <CHeaderNav class="d-md-down-none mr-auto">
+      <CHeaderNavItem class="px-3">
+        <CHeaderNavLink class="headerFont">          
+        </CHeaderNavLink>
+      </CHeaderNavItem>
+      <CHeaderNavItem class="px-3">
+        <CHeaderNavLink  class="headerFont" exact>       
+        </CHeaderNavLink>
+      </CHeaderNavItem>
+      <CHeaderNavItem class="px-3">
+        <CHeaderNavLink  class="headerFont" >        
+        </CHeaderNavLink>
+      </CHeaderNavItem>
+    </CHeaderNav>
+    <CHeaderNav  >
+      <CHeaderNavItem class="px-3 c-d-legacy-none headerFont">
+        <button
+          @click="() => $store.commit('toggle', 'darkMode')"
+          class="c-header-nav-btn headerFont"
+        >
+          <CIcon  class="headerFont" v-if="$store.state.darkMode" name="cil-sun"/>
+          <CIcon  class="headerFont" v-else name="cil-moon"/>
+        </button>
+      </CHeaderNavItem>
+      <TheHeaderDropdownNotif />
+      <TheHeaderDropdownAccnt/>
+      <CHeaderNavItem class="px-3">
+        
+      </CHeaderNavItem>
+    </CHeaderNav>
+
+    <CSubheader class="px-3">
+      <CBreadcrumbRouter class="border-0 mb-0"/>
+    </CSubheader>
+  </CHeader>
+</template>
+
+<script>
+import TheHeaderDropdownAccnt from './TheHeaderDropdownAccnt'
+import TheHeaderDropdownNotif from './TheHeaderDropdownNotif'
+import TheHeaderDropdownTasks from './TheHeaderDropdownTasks'
+import TheHeaderDropdownMssgs from './TheHeaderDropdownMssgs'
+
+export default {
+  name: 'TheHeader',
+  components: {
+    TheHeaderDropdownAccnt,
+    TheHeaderDropdownNotif,
+    TheHeaderDropdownTasks,
+    TheHeaderDropdownMssgs
+  },
+  data(){
+    return{
+      iconflag:false
+    }    
+  },
+  computed: {
+    show () {
+      if(this.$store.state.sidebarShow){
+        this.iconflag=false
+        return '';
+      }
+      this.iconflag=true
+      return '';
+    },
+  }
+}
+</script>
+<style lang="scss">
+  @import '../assets/scss/style';
+</style>
---
+++ src/containers/TheSidebar.vue
@@ -0,0 +1,39 @@
+<template>
+  <CSidebar
+    :minimize="minimize"
+    unfoldable
+    :show="show"
+    @update:show="(value) => $store.commit('set', ['sidebarShow', value])"
+  >
+    <CSidebarBrand class="d-md-down-none" to="/">
+      <img src="logo.png"  class="c-sidebar-brand-full" style="width:70%"/>
+      <img src="logo.png"  class="c-sidebar-brand-minimized" style="width:50%"/>   
+    </CSidebarBrand>
+  
+    <CRenderFunction class="headerFont" flat :contentToRender="sidebarItems"/>
+    <CSidebarMinimizer
+      class="c-d-md-down-none"
+      @click.native="$store.commit('toggle', 'sidebarMinimize')"
+    />
+  </CSidebar>
+</template>
+
+<script>
+import SidebarItems from './menu'
+export default {
+  name: 'TheSidebar',
+  extends: SidebarItems,
+  computed: {
+    show () {
+      return this.$store.state.sidebarShow
+    },
+    minimize () {
+      return this.$store.state.sidebarMinimize
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '../assets/scss/style';
+</style>
