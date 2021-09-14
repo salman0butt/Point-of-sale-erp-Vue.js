95adb755c05e3d621e6a0ce140936d19ee38b0a4
Salman butt <salman.butt@switcher.app>
2021-09-14 17:33:22 +0500

    chnage pagination to backend pagination

--- src/services/designations/DesignationService.js
+++ src/services/designations/DesignationService.js
@@ -1,8 +1,15 @@
 import http from "../../http-common";
 
 class DesignationService {
-  getAll() {
-    return http.get("/designations");
+  getAll(page, per_page) {
+    let url = "/designations";
+    if(page !== '')
+     url = this.updateQueryStringParameter(url,"page",page);
+
+    if(per_page !== '')
+     url = this.updateQueryStringParameter(url,"per_page",per_page);
+
+    return http.get(url);
   }
 
   get(id) {
@@ -20,6 +27,18 @@ class DesignationService {
   delete(id) {
     return http.delete(`/designations/${id}`);
   }
+
+  updateQueryStringParameter(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+      return uri.replace(re, '$1' + key + "=" + value + '$2');
+    }
+    else {
+      return uri + separator + key + "=" + value;
+    }
+  }
+
 }
 
 export default new DesignationService();
