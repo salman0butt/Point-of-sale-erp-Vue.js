181eabf3657275f1e2e412d19d3f9d65456a1217
Salman butt <salman.butt@switcher.app>
2021-10-17 10:50:41 +0500

    added Salary payments

---
+++ src/components/employees/employeeSalaryPayment/EmployeeSalaryPaymentForm.vue
@@ -0,0 +1,248 @@
+<template>
+  <div>
+    <CRow>
+      <CCol xs="12" lg="12">
+        <form
+          @submit.prevent="
+            isEditing ? updateEmployeeSalaryPayment() : saveEmployeeSalaryPayment()
+          "
+        >
+          <CRow>
+            <CCol sm="6" md="6" class="pt-2">
+              <CSelect
+                label="Select Salary Adjustment"
+                :options="options.employee_salary_adjustments"
+                :value.sync="form.employee_salary_adjustment_id"
+              />
+              <div v-if="$v.form.employee_salary_adjustment_id.$error">
+                <p
+                  v-if="!$v.form.employee_salary_adjustment_id.required"
+                  class="errorMsg"
+                >
+                  Salary Adjustment is required
+                </p>
+              </div>
+            </CCol>
+            <CCol sm="6" md="6" class="pt-2">
+              <CSelect
+                label="Select Bank Account"
+                :options="options.bank_accounts"
+                :value.sync="form.bank_account_id"
+              />
+              <div v-if="$v.form.bank_account_id.$error">
+                <p v-if="!$v.form.bank_account_id.required" class="errorMsg">
+                  Bank Account is required
+                </p>
+              </div>
+            </CCol>
+            <CCol sm="6" md="6" class="pt-2">
+              <CInput
+                label="Date"
+                type="date"
+                v-model="form.date"
+                :class="{ error: $v.form.date.$error }"
+                @input="$v.form.date.$touch()"
+              />
+              <div v-if="$v.form.date.$error">
+                <p v-if="!$v.form.date.required" class="errorMsg">Date is required</p>
+              </div>
+            </CCol>
+            <CCol sm="6" md="6" class="pt-2">
+              <CInput
+                label="Amount Paid"
+                type="number"
+                placeholder="0.00"
+                v-model="form.amount_paid"
+                :class="{ error: $v.form.amount_paid.$error }"
+                @input="$v.form.amount_paid.$touch()"
+              />
+              <div v-if="$v.form.amount_paid.$error">
+                <p v-if="!$v.form.amount_paid.required" class="errorMsg">
+                  Amount Paid is required
+                </p>
+              </div>
+            </CCol>
+          </CRow>
+
+          <p v-if="$v.$anyError" class="errorMsg">Please Fill the required data</p>
+          <CRow class="mt-4 d-block">
+            <CButton
+              progress
+              timeout="2000"
+              block
+              color="success"
+              style="float: right; width: 150px; margin-right: 20px"
+              type="submit"
+              >Save</CButton
+            >
+          </CRow>
+        </form>
+      </CCol>
+    </CRow>
+  </div>
+</template>
+<script>
+import EmployeeSalaryPaymentService from "@/services/employees/EmployeeSalaryPaymentService";
+import { required } from "vuelidate/lib/validators";
+import { cibAddthis, cisMinusSquare } from "@coreui/icons-pro";
+
+export default {
+  name: "EmployeeBasicForm",
+  cibAddthis,
+  cisMinusSquare,
+  data: () => ({
+    isEditing: false,
+    form: {
+      id: null,
+      employee_id: "",
+      employee_salary_adjustment_id: "",
+      bank_account_id: "",
+      date: "",
+      amount_paid: "",
+    },
+    empId: "",
+    options: {
+      employee_salary_adjustments: [
+        { value: "", label: "Choose Salary Adjustment", disabled: true, selected: "" },
+      ],
+      bank_accounts: [
+        { value: "", label: "Choose Bank Account", disabled: true, selected: "" },
+      ],
+    },
+  }),
+  validations() {
+    return {
+      form: {
+        employee_id: { required },
+        employee_salary_adjustment_id: { required },
+        bank_account_id: { required },
+        date: { required },
+        amount_paid: { required },
+      },
+    };
+  },
+  created() {
+    this.empId = this.$route.params.id;
+
+    this.getOptions();
+  },
+  methods: {
+    saveEmployeeSalaryPayment() {
+      this.form.employee_id = this.$route.params.id;
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        let data = this.form;
+        EmployeeSalaryPaymentService.create(data)
+          .then((res) => {
+            if (res.status == 201) {
+              this.$swal.fire({
+                icon: "success",
+                title: "Success",
+                text: "Salary Payment Added Successfully",
+                timer: 3600,
+              });
+              this.$v.$reset();
+              this.$emit("employee-salary-payment-update");
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            this.$swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Something Went Wrong.",
+              timer: 3600,
+            });
+          });
+      }
+    },
+    updateEmployeeSalaryPayment() {
+      this.form.employee_id = this.$route.params.id;
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        let data = this.form;
+        EmployeeSalaryPaymentService.update(this.form.id, data)
+          .then((res) => {
+            if (res.status == 200) {
+              this.$swal.fire({
+                icon: "success",
+                title: "Success",
+                text: "Salary Payment Updated Successfully",
+                timer: 3600,
+              });
+              this.$v.$reset();
+              this.$emit("employee-salary-payment-update");
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            this.$swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Something Went Wrong.",
+              timer: 3600,
+            });
+          });
+      }
+    },
+    getOptions() {
+      EmployeeSalaryPaymentService.getAllOptions()
+        .then(({ data }) => {
+          if (data != undefined && data != "") {
+            const employee_salary_adjustments = this.options.employee_salary_adjustments;
+            const bank_accounts = this.options.bank_accounts;
+            if (data.employee_salary_adjustments) {
+              data.employee_salary_adjustments.map(function (val) {
+                employee_salary_adjustments.push({ value: val.uuid, label: val.month });
+              });
+            }
+            if (data.bank_accounts) {
+              data.bank_accounts.map(function (val) {
+                bank_accounts.push({ value: val.uuid, label: val.name });
+              });
+            }
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getEmployeeSalaryPayment() {
+      EmployeeSalaryPaymentService.get(this.empId)
+        .then(({ data }) => {
+          if (data != null && data != "") {
+            this.isEditing = true;
+            this.form.id = data.uuid;
+            this.form.salary_type = data.employee.uuid;
+            this.form.employee_salary_adjustment_id =
+              data.employee_salary_adjustment.uuid;
+            this.form.bank_account_id = data.bank_account.uuid;
+            this.form.date = data.date;
+            this.form.amount_paid = data.amount_paid;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isEditing = false;
+        });
+    },
+    getEditData(uuid) {
+      this.isEditing = true;
+      this.empId = uuid;
+      this.getEmployeeSalaryPayment();
+    },
+    resetForm() {
+      for (let index in this.form) {
+        this.form[index] = "";
+      }
+      this.isEditing = false;
+    },
+  },
+};
+</script>
+
+<style>
+.errorMsg {
+  color: red;
+}
+</style>
---
+++ src/components/employees/employeeSalaryPayment/EmployeeSalaryPaymentIndex.vue
@@ -0,0 +1,249 @@
+<template>
+  <div>
+    <CRow>
+      <CCol xs="12" lg="12">
+        <CCard>
+          <CCardBody>
+            <CDataTable
+              :items="employeeSalaryPayment"
+              :fields="fields"
+              table-filter
+              items-per-page-select
+              @pagination-change="changePagination"
+              :items-per-page="perPage"
+              sorter
+              pagination
+              clickable-rows
+              hover
+              :loading="loading"
+              @row-clicked="rowClicked"
+            >
+              <template #select="{ item }">
+                <td>
+                  <CInputCheckbox
+                    :checked="item._selected"
+                    @update:checked="() => check(item)"
+                    custom
+                  />
+                </td>
+              </template>
+              <template #employee="{ item }">
+                <td>
+                  {{ item.employee.full_name }}
+                </td>
+              </template>
+              <template #employee_salary_adjustment="{ item }">
+                <td>
+                  {{ item.employee_salary_adjustment.month }}
+                </td>
+              </template>
+              <template #bank_account="{ item }">
+                <td>
+                  {{ item.bank_account.name }}
+                </td>
+              </template>
+
+              <template #actions="{ item }">
+                <td>
+                  <CButtonGroup>
+                    <!-- <CButton
+                      @click="viewRow(item.uuid)"
+                      class="btn-sm"
+                      color="success"
+                      title="View"
+                      >View</CButton
+                    > -->
+                    <CButton
+                      @click="editRow(item.uuid)"
+                      class="btn-sm text-white"
+                      color="warning"
+                      title="Edit"
+                    >
+                      <CIcon :content="$options.cilPencil"
+                    /></CButton>
+                    <CButton
+                      @click="deleteRow(item.uuid)"
+                      class="btn-sm"
+                      color="danger"
+                      title="Delete"
+                    >
+                      <CIcon :content="$options.cilTrash" />
+                    </CButton>
+                  </CButtonGroup>
+                </td>
+              </template>
+            </CDataTable>
+            <CPagination
+              v-show="pages > 1"
+              :pages="pages"
+              :active-page.sync="activePage"
+            />
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  </div>
+</template>
+
+<script>
+import EmployeeSalaryPaymentService from "@/services/employees/EmployeeSalaryPaymentService";
+import { cilPencil, cilTrash, cilEye } from "@coreui/icons-pro";
+
+const fields = [
+  // {
+  //   key: "select",
+  //   label: "",
+  //   _style: "min-width:1%",
+  //   sorter: false,
+  //   filter: false,
+  // },
+  { key: "employee", label: "EMPLOYEE", _style: "min-width:40%" },
+  {
+    key: "employee_salary_adjustment",
+    label: "SALARY ADJUSTMENT MONTH",
+    _style: "min-width:15%;",
+  },
+  { key: "bank_account", label: "BANK ACCOUNT", _style: "min-width:15%;" },
+  { key: "date", label: "DATE", _style: "min-width:15%;" },
+  { key: "amount_paid", label: "AMOUNT PAID", _style: "min-width:15%;" },
+  { key: "actions", label: "ACTION", _style: "min-width:15%;" },
+];
+
+export default {
+  name: "EmployeeSalaryPaymentIndex",
+  cilPencil,
+  cilTrash,
+  cilEye,
+  data() {
+    return {
+      employeeSalaryPaymentData: [],
+      fields,
+      loading: false,
+      deleteRows: [],
+      empId: null,
+      activePage: 1,
+      pages: 0,
+      perPage: 10,
+    };
+  },
+  created() {
+    this.loading = true;
+    this.getEmployeeSalaryPayment();
+  },
+  computed: {
+    employeeSalaryPayment() {
+      return this.employeeSalaryPaymentData;
+    },
+  },
+  watch: {
+    reloadParams() {
+      this.onTableChange();
+    },
+    activePage() {
+      this.getEmployeeSalaryPayment(this.activePage, this.perPage);
+    },
+  },
+  methods: {
+    getEmployeeSalaryPayment(page = "", per_page = "") {
+      this.empId = this.$route.params.id;
+
+      EmployeeSalaryPaymentService.getAll(this.empId, page, per_page)
+        .then(({ data }) => {
+          console.log(data);
+          if (data !== "" && data !== undefined) {
+            this.loading = true;
+            this.employeeSalaryPaymentData = [];
+            if (data.data) {
+              data.data.map((item, id) => {
+                this.employeeSalaryPaymentData.push({ ...item, id });
+              });
+            }
+            if (data.meta) {
+              this.setPagination(data.meta);
+            }
+          }
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    rowClicked(item, index, column, e) {
+      if (!["INPUT", "LABEL"].includes(e.target.tagName)) {
+        this.check(item);
+      }
+    },
+    check(item) {
+      const val = Boolean(this.employeeSalaryPaymentData[item.id]._selected);
+      this.$set(this.employeeSalaryPaymentData[item.id], "_selected", !val);
+    },
+    viewRow(uuid) {
+      alert("page not ready");
+    },
+    editRow(uuid) {
+      this.$emit("employee-salary-payment-edit", uuid);
+    },
+
+    deleteRow(uuid) {
+      this.deleteRows = JSON.stringify([uuid]);
+      this.$swal
+        .fire({
+          title: "Do you want to delete this record",
+          text: "This will be record from Database",
+          showCancelButton: true,
+          confirmButtonColor: "#e55353",
+          confirmButtonText: "Yes, remove it it!",
+        })
+        .then((result) => {
+          if (result.isConfirmed) {
+            EmployeeSalaryPaymentService.delete(this.deleteRows)
+              .then((res) => {
+                if (res.status == 200) {
+                  this.$swal.fire({
+                    icon: "success",
+                    title: "Success",
+                    text: "SalaryPayment Deleted Successfully",
+                    timer: 3600,
+                  });
+                  this.employeeSalaryPaymentData = this.employeeSalaryPaymentData.filter(
+                    (department) => department.uuid != uuid
+                  );
+                  this.deleteRows = [];
+                }
+              })
+              .catch((error) => {
+                this.$swal.fire({
+                  icon: "error",
+                  title: "Error",
+                  text: "Something went Wrong",
+                  timer: 3600,
+                });
+              });
+          }
+        });
+    },
+    setPagination(meta) {
+      this.activePage = parseInt(meta.current_page);
+      this.pages = parseInt(meta.last_page);
+      this.perPage = parseInt(meta.per_page);
+    },
+    onTableChange() {
+      setTimeout(() => {
+        this.loading = false;
+        const agent = this.$refs.externalAgent;
+        this.employeeSalaryPaymentData = agent.currentItems;
+        this.pages = Math.ceil(agent.sortedItems.length / 5);
+      }, 1000);
+    },
+    changePagination(value) {
+      this.perPage = parseInt(value);
+      this.getEmployeeSalaryPayment("", this.perPage);
+    },
+  },
+};
+</script>
+<style scoped>
+.bolder {
+  font-weight: 600;
+}
+</style>
