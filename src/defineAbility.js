7b228ec136329a112aa5cfc7fe8340443f6aa484
Salman butt <salman.butt@switcher.app>
2021-10-23 12:08:07 +0500

    permissions

---
+++ src/defineAbility.js
@@ -0,0 +1,15 @@
+import { defineAbility } from '@casl/ability';
+import store from './store'
+
+export default defineAbility((can, cannot) => {
+
+  let permissions = store.state.permissions;
+  // console.log('test',permissions);
+  permissions.map(function(item) {
+     let permission = item.split(" ");
+    can(permission[0], permission[1]);
+  });
+  // can('manage', 'designation');
+  // can('delete', 'designation');
+  // cannot('delete', 'User');
+});
