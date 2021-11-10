6fa9a4b2e4037efd3e42c13caf40a8c16aeb3145
khurram474358 <khurram474358@gmail.com>
2021-11-10 14:30:56 +0300

    changges revert

--- src/http-common.js
+++ src/http-common.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://sw-backend.test",
-  // baseURL: "http://switcher.one",
+  // baseURL: "http://sw-backend.test",
+  baseURL: "http://switcher.one",
   headers: {
     "Accept": "application/json",
     "Content-type": "application/json",
-    // "X-Tenant": window.location.hostname,
-    "X-Tenant": "demo1.sw-backend.test",
+    "X-Tenant": window.location.hostname,
+    // "X-Tenant": "demo1.sw-backend.test",
     "Cache-Control": "no-cache",
 
   }
