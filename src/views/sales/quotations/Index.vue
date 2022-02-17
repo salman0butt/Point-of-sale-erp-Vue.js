5e6bbb0669edf1d69493d7b9755351831fdb0b2c
khurram474358 <khurram474358@gmail.com>
2022-02-17 14:46:23 +0300

    add customer added

--- src/views/sales/quotations/Index.vue
+++ src/views/sales/quotations/Index.vue
@@ -204,7 +204,9 @@ export default {
                     text: "Quotation Deleted Successfully",
                     timer: 3600,
                   });
-                  this.serverData = this.serverData.filter((item) => item.uuid != uuid);
+                  this.serverData = this.serverData.filter(
+                    (item) => item.uuid != uuid
+                  );
                 }
               })
               .catch((error) => {
