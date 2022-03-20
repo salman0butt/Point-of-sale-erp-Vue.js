bf087b0619946e80c57eef6a08b477feb60a4490
khurram474358 <khurram474358@gmail.com>
2022-03-20 11:41:21 +0300

    profit and loss completed

--- src/components/reports/ProfitAndLossReport.vue
+++ src/components/reports/ProfitAndLossReport.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <Loader />
+
     <CCard>
       <CCardHeader>
         Product Report
@@ -77,24 +79,93 @@
     <CCard>
       <CCardHeader> Result </CCardHeader>
       <CCardBody>
-        <div>
-          <CDataTable
-            :items="items"
-            :fields="fields"
-            table-filter
-            items-per-page-select
-            sorter
-            clickable-rows
-            hover
-            ref="externalAgent"
-          >
-          </CDataTable>
-          <CPagination
-            v-show="pages > 1"
-            :pages="pages"
-            :active-page.sync="activePage"
-          />
-        </div>
+        <CCardBody id="printMe">
+          <CRow class="mb-4">
+            <CCol col="12">
+              <CImg
+                v-bind:src="business.logo"
+                block
+                align="center"
+                class="text-center"
+                width="100%"
+                style="width: 200px; height: 200px"
+              />
+              <h2 align="center">Profit And Loss</h2>
+              <h4 align="center">From Date to Date</h4>
+            </CCol>
+          </CRow>
+          <CRow class="mb-4">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th class="right">
+                    {{ $t("profitandloss.account") }}
+                  </th>
+                  <th class="center">
+                    {{ $t("profitandloss.accountcode") }}
+                  </th>
+                  <th class="right">
+                    {{ $t("profitandloss.total") }}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="income in allIncomes" :key="income.id">
+                  <td class="left" v-if="income.account">
+                    {{
+                      income.account.name
+                        ? income.account.name
+                        : "Account Name Missing"
+                    }}
+                  </td>
+                  <td class="center" v-if="income.account">
+                    {{ income.account.code ? income.account.code : "-" }}
+                  </td>
+                  <td class="right">
+                    {{ income.ledger[0] ? income.ledger[0].total_credit : "-" }}
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="2"><h5>TOTAL INCOME</h5></td>
+                  <td>
+                    <h5>{{ total_income }}</h5>
+                  </td>
+                </tr>
+
+                <tr v-for="expense in allExpenses" :key="expense.id">
+                  <td class="left" v-if="expense.account">
+                    {{
+                      expense.account.name
+                        ? expense.account.name
+                        : "Account Name Missing"
+                    }}
+                  </td>
+                  <td class="center" v-if="expense.account">
+                    {{ expense.account.code ? expense.account.code : "-" }}
+                  </td>
+                  <td class="right">
+                    {{
+                      expense.ledger[0] ? expense.ledger[0].total_debit : "-"
+                    }}
+                  </td>
+                </tr>
+                <tr>
+                  <td colspan="2"><h5>TOTAL EXPENSE</h5></td>
+                  <td>
+                    <h5>{{ total_expense }}</h5>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td colspan="2"><h4>Profit & Loss</h4></td>
+                  <td>
+                    <h5>{{ profit_and_loss }}</h5>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </CRow>
+        </CCardBody>
       </CCardBody>
     </CCard>
   </div>
@@ -103,42 +174,7 @@
 import { cibAddthis, cisMinusSquare } from "@coreui/icons-pro";
 import { tableMixin } from "@/mixins/tableMixin";
 import ReportService from "@/services/reports/ReportService";
-
-const fields = [
-  { key: "product_name", label: "PRODUCT NAME", _style: "width:50%" },
-  { key: "product_code", label: "PRODUCT CODE", _style: "width:50%" },
-  { key: "supplier", label: "SUPPLIER", _style: "width:50%" },
-  { key: "barcode", label: "BARCODE", _style: "width:50%" },
-  { key: "type", label: "TYPE", _style: "width:50%" },
-  { key: "category", label: "CATEGORY", _style: "width:50%" },
-  {
-    key: "current_cost_price",
-    label: "CURRENT COST PRICE",
-    _style: "width:50%",
-  },
-  {
-    key: "current_selling_price",
-    label: "CURRENT SELLING PRICE",
-    _style: "width:50%",
-  },
-  {
-    key: "current_profit_price",
-    label: "CURRENT PROFIT PRICE",
-    _style: "width:50%",
-  },
-  { key: "tax_type", label: "TAX TYPE", _style: "width:50%" },
-  { key: "quantity", label: "QUANTITY", _style: "width:50%" },
-  {
-    key: "quantity_purchased",
-    label: "QUANTITY PURCHASED",
-    _style: "width:50%",
-  },
-  { key: "quantity_balance", label: "QUANTITY BALANCE", _style: "width:50%" },
-  { key: "quantity_damage", label: "QUANTITY DAMAGE", _style: "width:50%" },
-  { key: "quantity_return", label: "QUANTITY RETURN", _style: "width:50%" },
-  { key: "expire_date", label: "EXPIRE DATE", _style: "width:50%" },
-  { key: "weight_unit", label: "WEIGHT UNIT", _style: "width:50%" },
-];
+import Loader from "@/components/layouts/Loader.vue";
 
 export default {
   name: "ProfitAndLossReport",
@@ -146,48 +182,11 @@ export default {
   cibAddthis,
   cisMinusSquare,
   data: () => ({
-    perPage: 25,
-    data: [
-      {
-        product_name: "Product 1",
-        product_code: "P001",
-        supplier: "Supplier 1",
-        barcode: "B001",
-        type: "Type 1",
-        category: "Category 1",
-        current_cost_price: "100",
-        current_selling_price: "200",
-        current_profit_price: "100",
-        tax_type: "Tax Type 1",
-        quantity: "100",
-        quantity_purchased: "100",
-        quantity_balance: "100",
-        quantity_damage: "100",
-        quantity_return: "100",
-        expire_date: "2020-01-01",
-        weight_unit: "Weight Unit 1",
-      },
-      {
-        product_name: "Product 2",
-        product_code: "P002",
-        supplier: "Supplier 2",
-        barcode: "B002",
-        type: "Type 2",
-        category: "Category 2",
-        current_cost_price: "100",
-        current_selling_price: "200",
-        current_profit_price: "100",
-        tax_type: "Tax Type 2",
-        quantity: "100",
-        quantity_purchased: "100",
-        quantity_balance: "100",
-        quantity_damage: "100",
-        quantity_return: "100",
-        expire_date: "2020-01-01",
-        weight_unit: "Weight Unit 2",
-      },
-    ],
-    fields,
+    branch: Object,
+    business: {
+      logo: "",
+    },
+    payment: Object,
     toggleOptions: true,
     form: {
       date_range: "",
@@ -213,31 +212,84 @@ export default {
         { value: "custom_date_range", label: "Custom Date Range" },
       ],
     },
+    total_income: 0,
+    total_expense: 0,
+    profit_and_loss: 0,
+    incomes: [],
+    expenses: [],
   }),
   created() {
     this.getServerData();
   },
+  components: {
+    Loader,
+  },
   computed: {
-    items() {
-      return this.data;
+    allIncomes() {
+      return this.incomes;
+    },
+    allExpenses() {
+      return this.expenses;
     },
   },
   methods: {
-    getServerData() {
-      // console.log("test");
-      ReportService.profitAndLossReport()
-        .then(function (data) {
-          console.log(data);
-        })
-        .catch(function (error) {
-          console.log("error");
-        });
-    },
+    // async print() {
+    //   await this.$htmlToPaper("printMe");
+    // },
+    // savePdf() {
+    //   this.$refs.html2Pdf.generatePdf();
+    // },
     toggleSection() {
       this.toggleOptions = !this.toggleOptions;
     },
     genrateReport() {
       this.toggleSection();
+    },
+    getServerData() {
+      let income_server = this.incomes;
+      let expense_server = this.expenses;
+      ReportService.profitAndLossReport()
+        .then(function ({ data }) {
+          if (data && data.income) {
+            var total_income = 0;
+            data.income.map(function (value) {
+              income_server.push(value);
+              if (value.ledger.length > 0) {
+                total_income += parseFloat(value.ledger[0].total_credit);
+              }
+              total_income.toFixed(3);
+            });
+          }
+          if (data && data.expense) {
+            var total_expense = 0;
+            data.expense.map(function (value) {
+              expense_server.push(value);
+              if (value.ledger.length > 0) {
+                total_expense += parseFloat(value.ledger[0].total_debit);
+              }
+            });
+            total_expense.toFixed(3);
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+      // Business logo
+      this.$store.commit("set_loader");
+      let business_id = localStorage.getItem("business_id");
+      this.$http
+        .get("/business/" + business_id)
+        .then(({ data }) => {
+          if (data.logo && data.logo.path) {
+            this.business.logo = data.logo.path;
+          }
+          this.$store.commit("close_loader");
+        })
+        .catch((err) => {
+          this.$store.commit("close_loader");
+          console.log(err);
+        });
     },
   },
 };
